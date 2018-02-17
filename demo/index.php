fb09f14c41adfff8a03ef7a5f07b789b7f90abce
Johan Mårtensson <johanmartensson@gmail.com>
2018-02-17 18:27:44 +0100

    Updated .gitignore and demo cleanup

--- demo/index.php
+++ demo/index.php
@@ -6,6 +6,7 @@
     <title>Dev-Tests - ics</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment-with-locales.min.js" integrity="sha256-XWrGUqSiENmD8bL+BVeLl7iCfhs+pkPyIqrZQcS2Te8=" crossorigin="anonymous"></script>
+    <script src="js/filesaver.min.js"></script>
     <script src="js/ics.js"></script>
 </head>
 
